3bdb6df539d139d3cdb036f9650c666ff9081c81
fezick <jason@solefamily.net>
2013-03-03 10:17:57 -0500

    Fix bank update bug in save and add post_delete signal to DietDay for updating the bank.

--- CalorieBank/calorie_bank/models.py
+++ CalorieBank/calorie_bank/models.py
@@ -1,17 +1,32 @@
+#from django.core.signals import request_finished
+from django.dispatch import receiver
+from django.db.models.signals import post_delete
 from django.db import models
 
 # Create your models here.
 class DietDay(models.Model):
-    date = models.DateTimeField()
+    date = models.DateField()
     calories = models.IntegerField()
     
     def save(self):
-        bank = CalorieBank.objects.all()[0]
-        bank.save()
+        bank = CalorieBank.objects.all()[0]        
         super(DietDay,self).save()
+        bank.save()
+    
+    """def delete():
+        bank = CalorieBank.objects.all()[0]        
+        super(delete,self).delete()
+        bank.save()"""
     
     def __unicode__(self):
         return "%s (%s)" % (self.date, self.calories)
+
+@receiver(post_delete)
+def update_bank(sender, **kwargs):
+    bank = CalorieBank.objects.all()[0]
+    bank.save()
+
+post_delete.connect(update_bank, sender=DietDay)
 
 class CalorieBank(models.Model):
     balance = models.IntegerField()
@@ -27,6 +42,7 @@ class CalorieBank(models.Model):
                 remainder = 200
             bal += remainder
         self.balance=bal
+        print bal
         super(CalorieBank,self).save()
 
     def __unicode__(self):
